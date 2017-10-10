094b48943ce2fc7d26f6c24a087f22b0585348b7
rainkinz <brendan.grainger@gmail.com>
2017-10-10 06:08:05 -0400

    add local aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Allow for 'local', i.e. not checked into dotfiles repo aliases
+if [ -e "$HOME/.aliases.local" ] ; then
+  source "$HOME/.aliases.local"
+fi
