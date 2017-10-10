5235ea3efd394de21cac954bbaa3c630201a247f
rainkinz <brendan.grainger@gmail.com>
2017-10-10 06:22:15 -0400

    fixed merge conflict and downgrade to java8

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-<<<<<<< HEAD
 
 # Allow for 'local', i.e. not checked into dotfiles repo aliases
 if [ -e "$HOME/.aliases.local" ] ; then
   source "$HOME/.aliases.local"
 fi
-=======
->>>>>>> 7eecf9daeb8b12935d7c7dbdfe5c9ace1e807b71
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -37,3 +37,6 @@ bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+bindkey '^b' backward-char
+bindkey '^f' forward-char
